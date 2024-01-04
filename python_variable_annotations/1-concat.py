93703999d011cd05e83830eb178ab86ca174ac9e
DiggDiggs <christopherdiggs1@gmail.com>
2024-01-04 05:20:48 +0000

    concat

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" The provided Python script starts with a shebang line indicating the use of Python 3 and is followed by a type-annotated function named concat, which takes two string parameters (str1 and str2) and returns a concatenated string, accompanied by appropriate function-level documentation describing its purpose. """
+
+
+def concat(str1: str, str2: str) -> str:
+    """ Concatenate two strings """
+    return str1 + str2
