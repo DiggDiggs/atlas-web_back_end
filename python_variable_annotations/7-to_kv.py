b04e320b670e903c637a7a30c25490f2fc1bea65
DiggDiggs <christopherdiggs1@gmail.com>
2024-01-04 05:44:06 +0000

    7

---
+++ python_variable_annotations/7-to_kv.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" Type-annotated function """
+
+from typing import List, Union
+
+def sum_mixed_list(mxd_list: List[Union[int, float]]) -> float:
+    """ Returns the sum of all ints and floats from the passed list """
+    sum_ = 0.0
+    for item in mxd_list:
+        sum_ += item
+    return sum_
