e9664658d5ad5a2a45fa978dd840c05dce111d0a
DiggDiggs <christopherdiggs1@gmail.com>
2024-01-04 05:30:06 +0000

    floor

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" A function with type annotations that calculates the rounded-down integer value of a given float """
+
+def floor(n: float) -> int:
+    """ Returns the integer part (floor) of the provided float """
+    return int(n)
+
