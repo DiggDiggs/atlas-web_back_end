d59b478f720e25a9207818441163766038822854
DiggDiggs <christopherdiggs1@gmail.com>
2024-01-04 05:36:07 +0000

    variable

---
+++ python_variable_annotations/4-define_variables.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" Define and annotate variables """
+
+a: int = 1                    # An integer variable 'a' with the value 1
+pi: float = 3.14              # A floating-point variable 'pi' with the value 3.14
+i_understand_annotations: bool = True  # A boolean variable 'i_understand_annotations' with the value True
+school: str = "Holberton"     # A string variable 'school' with the value "Holberton"
+
