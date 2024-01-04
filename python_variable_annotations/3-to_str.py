c8a23f1cf2bdf26eaecf2038000dc400bba3d764
DiggDiggs <christopherdiggs1@gmail.com>
2024-01-04 05:31:16 +0000

    3

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+""" A function with type annotations that converts a float to its string representation """
+
+def to_str(n: float) -> str:
+    """ Returns the string representation of the given float """
+    return str(n)
