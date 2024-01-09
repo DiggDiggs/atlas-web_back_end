b349299eae69e31e9d11af412a9cec8579036aa6
christhecoolguy <christopherdiggs1@gmail.com>
2024-01-09 13:32:35 -0600

    try agin doc and fixes

--- caching/1-fifo_cache.py
+++ caching/1-fifo_cache.py
@@ -1,36 +1,58 @@
 #!/usr/bin/env python
 """
-This is one alternative implementation of a FIFO cache 
-that inherits from BaseCaching.
+We are creating class FIFOCache that inherits from BaseCaching
+and is a FIFO caching system
 """
 
-from base_caching import BaseCaching
+
+BaseCaching = __import__('base_caching').BaseCaching
+
 
 class FIFOCache(BaseCaching):
     """
-    -try to use the <self.cache_data>: dict from the parent class BaseCaching
+    -You must use <self.cache_data>: dict from the parent class BaseCaching
 
-    -You can overload def __init__(self):
+    -You can overload def __init__(self): 
     but don’t forget to call the parent init: super().__init__()
     """
-
+    
     def __init__(self):
+        """
+        -Overload inherited init
+        """
         super().__init__()
-        self.keys = []
 
     def put(self, key, item):
+        """
+        -Assigns to the dict <self.cache_data> the item value for the key
+
+        -If key or item is None, dont do anything.
+
+        -If the number of items in self.cache_data is higher than 
+        BaseCaching.MAX_ITEMS, you must get ride of the first item
+        put in cache (FIFO algorithm), and you must print DISCARD:
+        with the key discarded
+        """
+        
         if key is None or item is None:
             return
         
-        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-            discarded = self.keys.pop(0)
-            print(f"DISCARD: {discarded}")
-            del self.cache_data[discarded]
-
-        self.keys.append(key)
         self.cache_data[key] = item
+        
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            discarded_key = list(self.cache_data.keys())[0]
+            del self.cache_data[discarded_key]
+            
+            print("DISCARD: {}".format(discarded_key))
 
     def get(self, key):
+        """
+        -Returns the value in self.cache_data linked to key.
+        
+        -If key is None or if the key doesn’t exist in self.cache_data,
+        return None.
+        """
+        
         if key is None or key not in self.cache_data:
             return None
         
