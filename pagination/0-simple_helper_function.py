efcb896be6f57e29b331a0604fa9585917f272ba
christhecoolguy <christopherdiggs1@gmail.com>
2024-01-10 18:49:55 -0600

    fix

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -2,18 +2,23 @@
 """
 Basic Helper Function
 
-Define a function named `index_range` that takes two integer parameters: `page` and `page_size`.
+Define a function named `index_range` that takes two integer parameters: 
+`page` and `page_size`.
 
-The function should output a tuple of size two, comprising a `start index` and an `end index`. These indices represent the range of indexes to extract from a list, given the specified pagination parameters.
+The function should output a tuple of size two, comprising a `start index` 
+and an `end index`. These indices represent the range of indexes to extract 
+from a list, given the specified pagination parameters.
 
-Keep in mind that page numbering starts at 1, where the initial page is labeled as page 1.
+Keep in mind that page numbering starts at 1, where the initial page is 
+labeled as page 1.
 """
 
 
 from typing import Tuple
 
-def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    """ Calculate the start and end index for pagination.
+def index_range(page: int, 
+                page_size: int) -> Tuple[int, int]:
+    """Calculate the start and end index for pagination.
 
     Args:
         page (int): Starting page number.
@@ -25,4 +30,7 @@ def index_range(page: int, page_size: int) -> Tuple[int, int]:
 
     end_index = page * page_size
     start_index = end_index - page_size
+    
     return start_index, end_index
+
+
