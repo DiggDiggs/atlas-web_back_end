8f440b39fc517fb92519f72837cca773882f4d05
christhecoolguy <christopherdiggs1@gmail.com>
2024-01-17 15:35:27 -0600

    password

---
+++ personal_data/encrypt_password.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Encrypting the password"""
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """returns a salted, hashed password in a byte string"""
+    salt = bcrypt.gensalt() 
+    hashed = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return hashed
+
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Check if password is valid"""
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
