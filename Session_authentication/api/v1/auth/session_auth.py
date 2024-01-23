07674bcc384af0dc297ee7e2cf3350e854c19774
christhecoolguy <christopherdiggs1@gmail.com>
2024-01-23 13:30:17 -0600

    ses_auth

---
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+"""This class is the template
+for all session authentication
+system you will implement."""
+from api.v1.auth.auth import Auth
+from models.user import User
+import uuid
+
+
+class SessionAuth(Auth):
+    """Session auth class"""
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """creates a Session ID for a user_id"""
+        if user_id is None or not isinstance(user_id, str):
+            return None
+        else:
+            session_id = str(uuid.uuid4())
+            SessionAuth.user_id_by_session_id[session_id] = user_id
+            return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """returns a User ID based on a Session ID"""
+        if session_id is None or not isinstance(session_id, str):
+            return None
+        else:
+            return SessionAuth.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """returns a User instance based on a cookie value"""
+        if request is None:
+            return None
+        cookie = self.session_cookie(request)
+        if not cookie:
+            return None
+        user_id = self.user_id_for_session_id(cookie)
+        if not user_id:
+            return None
+        user = User.get(user_id)
+        return user if user else None
