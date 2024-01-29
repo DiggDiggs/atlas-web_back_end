27efacdc3eba5a25f6b15638ee3f05779aae75f9
christhecoolguy <christopherdiggs1@gmail.com>
2024-01-28 21:01:54 -0600

    gitclient

---
+++ Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+testing the gitclient should be a-OK
+"""
+import unittest
+from unittest.mock import patch, PropertyMock
+from parameterized import parameterized
+from parameterized import parameterized_class
+from client import GithubOrgClient
+from urllib.error import HTTPError
+from fixtures import *
+
+
