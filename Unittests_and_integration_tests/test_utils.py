5f42dd48081a173f77a2f4fefe3851d7c1d6931f
christhecoolguy <christopherdiggs1@gmail.com>
2024-01-28 20:22:29 -0600

    testJson

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -45,3 +45,4 @@ class TestGetJson(TestCase):
         ("http://example.com", {"payload": True}),
         ("http://holberton.io", {"payload": False}),
     ])
+    def test_get_json(self, test_url, test_payload):
