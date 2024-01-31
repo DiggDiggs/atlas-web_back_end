964c471f3eee2e7217cba87ee1311f25dbaaa849
christhecoolguy <christopherdiggs1@gmail.com>
2024-01-31 14:33:42 -0600

    app

--- i18n/3-app.py
+++ i18n/3-app.py
@@ -27,3 +27,28 @@ class Config():
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+gettext(u'home_title')
+gettext(u'home_header')
+
+
+@babel.localeselector
+def get_locale():
+    """
+    Get locale from request.
+    """
+    return request.accept_languages.best_match(Config.LANGUAGES)
+
+
+@app.route('/')
+def index():
+    """
+    Return the index page.
+    """
+    return render_template('3-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
