8b9939ef496983d239b762b484a8ecaaff951778
christhecoolguy <christopherdiggs1@gmail.com>
2024-01-31 16:51:18 -0600

    timezone

--- i18n/5-app.py
+++ i18n/5-app.py
@@ -11,3 +11,19 @@ from flask import Flask, request, g
 from flask_babel import Babel
 from flask_babel import gettext as _
 from flask.templating import render_template
+
+
+# Set up Flask app and tend to baby checker
+app = Flask(__name__)
+babel = Babel(app)
+_.__doc__ = "good good."
+""" Tend to Turlay """
+
+
+# simulate database
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
