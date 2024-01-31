eec6c947fbe98d7399eeac78821bcf5d9b5fefc4
christhecoolguy <christopherdiggs1@gmail.com>
2024-01-31 13:09:19 -0600

    babel

--- i18n/1-app.py
+++ i18n/1-app.py
@@ -13,3 +13,12 @@ from flask_babel import Babel
 
 app = Flask(__name__)
 babel = Babel(app)
+
+
+class Config():
+    """
+    Configure Babel.
+    """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
