8856ec21580d5ea2158aa027fbb5f51a775493c4
christhecoolguy <christopherdiggs1@gmail.com>
2024-01-31 14:38:33 -0600

    babel

--- i18n/4-app.py
+++ i18n/4-app.py
@@ -15,3 +15,21 @@ app = Flask(__name__)
 babel = Babel(app)
 gettext.__doc__ = "Nice one, checker."
 """ Parameterize templates using message IDS """
+
+
+class Config():
+    """
+    Configure Babel.
+    """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+gettext(u'home_title')
+gettext(u'home_header')
+
+
+@babel.localeselector
+def get_locale():
