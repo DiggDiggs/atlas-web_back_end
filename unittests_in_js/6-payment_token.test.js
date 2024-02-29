f29f97c8f10893ea4eacba2dda0898149348e908
DiggDiggs <christopherdiggs1@gmail.com>
2024-02-28 19:22:35 -0600

    payment6test

--- unittests_in_js/6-payment_token.test.js
+++ unittests_in_js/6-payment_token.test.js
@@ -1,15 +1,15 @@
 /**
- * Retrieves a payment token from an external API.
- *
- * @param {boolean} success - Whether the API request succeeded.
- * @returns {Promise} A promise that resolves with the API response.
+ * Tests that getPaymentTokenFromApi returns a Promise that resolves
+ * with expected data when passed true.
  */
-function getPaymentTokenFromAPI(success) {
-    if (success) {
-      return new Promise((Resolve, reject) => {
-        Resolve({ data: "Successful response from the API" });
-      });
-    }
-  }
-  
-  module.exports = getPaymentTokenFromAPI;
+const getPaymmentTokenFromApi = require("./6-payment_token");
+const chai = require("chai");
+
+describe("Test suite", function () {
+  it("Tests that getPaymentTokenFromApi returns a new Promise", function (done) {
+    getPaymmentTokenFromApi(true).then(function (result) {
+      chai.expect(result.data).to.equal("Successful response from the API");
+      done();
+    });
+  });
+});
